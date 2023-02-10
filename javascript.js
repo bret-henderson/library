44db95032d81e4271a50ab628551248102900446
bret-henderson <98489557+bret-henderson@users.noreply.github.com>
2023-02-10 16:24:29 -0500

    Add new book functionality

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const cardContainer = document.querySelector(".card-container");
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -7,53 +8,83 @@ function Book(title, author, pages, read) {
   this.isRead = read;
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', true);
-const dune = new Book('Dune', 'Frank Herbert', '412', false);
+function addBookToLibrary(newBook) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  cardContainer.appendChild(card);
+
+  const title = document.createElement("h1");
+  title.classList.add("title");
+  title.textContent = newBook.title;
+  card.appendChild(title);
+
+  const author = document.createElement("h2");
+  author.classList.add("author");
+  author.textContent = newBook.author;
+  card.appendChild(author);
+
+  const pages = document.createElement("div");
+  pages.classList.add("pages");
+  pages.textContent = `${newBook.pages} pages`;
+  card.appendChild(pages);
+
+  const isRead = document.createElement("button");
+  isRead.classList.add("is-read");
+  isRead.textContent = newBook.isRead;
+  card.appendChild(isRead);
+}
+
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", true);
+const dune = new Book("Dune", "Frank Herbert", "412", false);
 
 // console.log(theHobbit.info());
 myLibrary.push(theHobbit);
 myLibrary.push(dune);
 
-const cardContainer = document.querySelector('.card-container');
+myLibrary.forEach((book) => {
+  addBookToLibrary(book);
+});
 
-function displayBooks() {
-  myLibrary.forEach((book) => {
-    const card = document.createElement('div');
-    card.classList.add('card');
-    cardContainer.appendChild(card);
-
-    const title = document.createElement('h1');
-    title.classList.add('title');
-    title.textContent = book.title;
-    card.appendChild(title);
-
-    const author = document.createElement('h2');
-    author.classList.add('author');
-    author.textContent = book.author;
-    card.appendChild(author);
-
-    const pages = document.createElement('div');
-    pages.classList.add('pages');
-    pages.textContent = `${book.pages} pages`;
-    card.appendChild(pages);
-
-    const isRead = document.createElement('button');
-    isRead.classList.add('is-read');
-    isRead.textContent = book.isRead;
-    card.appendChild(isRead);
-  });
-}
-
-displayBooks();
-
-const addBook = document.querySelector('.add-book');
+const addBook = document.querySelector(".add-book");
+const popup = document.querySelector(".popup-container");
 
 function openForm() {
-  addBook.classList.add('display: flex');
+  popup.style.display = "flex";
 }
 
-addBook.addEventListener('click', () => openForm());
+addBook.addEventListener("click", () => openForm());
 
-// function addBookToLibrary() {
-//   const newBook = {};
-// }
+popup.addEventListener("click", (e) => {
+  if (
+    e.target.className === "popup-container" ||
+    e.target.classList.contains("close-popup")
+  ) {
+    popup.style.display = "none";
+  }
+});
+
+const newBookForm = document.querySelector(".new-book-form");
+
+function submitForm(formData) {
+  const formDataObj = Object.fromEntries(formData);
+  const newBook = new Book(
+    formDataObj.title,
+    formDataObj.author,
+    formDataObj.pages,
+    formDataObj["read-checkbox"] === "on"
+  );
+  myLibrary.push(newBook);
+  addBookToLibrary(newBook);
+}
+
+function getData(form) {
+  const formData = new FormData(form);
+  // console.log(Object.fromEntries(formData));
+  submitForm(formData);
+}
+
+newBookForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getData(e.target);
+  popup.style.display = "none";
+});
