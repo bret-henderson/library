045f37a10d8edb2b3e43d1f70e699baeaf163e61
bret-henderson <98489557+bret-henderson@users.noreply.github.com>
2023-02-10 18:51:20 -0500

    Add remove book and read button to cards

--- javascript.js
+++ javascript.js
@@ -1,11 +1,19 @@
 const myLibrary = [];
 const cardContainer = document.querySelector(".card-container");
 
+const addBook = document.querySelector(".add-book");
+const popup = document.querySelector(".popup-container");
+
+const newBookForm = document.querySelector(".new-book-form");
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.isRead = read;
+  let readStr = "";
+  if (read === true) readStr = "Read";
+  else readStr = "Not Read";
+  this.isRead = readStr;
 }
 
 function addBookToLibrary(newBook) {
@@ -32,38 +40,16 @@ function addBookToLibrary(newBook) {
   isRead.classList.add("is-read");
   isRead.textContent = newBook.isRead;
   card.appendChild(isRead);
+
+  const removeBookBtn = document.createElement("button");
+  removeBookBtn.classList.add("remove-book");
+  removeBookBtn.textContent = "Remove Book";
+  card.appendChild(removeBookBtn);
 }
-
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", true);
-const dune = new Book("Dune", "Frank Herbert", "412", false);
-
-// console.log(theHobbit.info());
-myLibrary.push(theHobbit);
-myLibrary.push(dune);
-
-myLibrary.forEach((book) => {
-  addBookToLibrary(book);
-});
-
-const addBook = document.querySelector(".add-book");
-const popup = document.querySelector(".popup-container");
 
 function openForm() {
   popup.style.display = "flex";
 }
-
-addBook.addEventListener("click", () => openForm());
-
-popup.addEventListener("click", (e) => {
-  if (
-    e.target.className === "popup-container" ||
-    e.target.classList.contains("close-popup")
-  ) {
-    popup.style.display = "none";
-  }
-});
-
-const newBookForm = document.querySelector(".new-book-form");
 
 function submitForm(formData) {
   const formDataObj = Object.fromEntries(formData);
@@ -79,12 +65,71 @@ function submitForm(formData) {
 
 function getData(form) {
   const formData = new FormData(form);
-  // console.log(Object.fromEntries(formData));
   submitForm(formData);
 }
 
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", true);
+const dune = new Book("Dune", "Frank Herbert", "412", false);
+
+myLibrary.push(theHobbit);
+myLibrary.push(dune);
+
+myLibrary.forEach((book) => {
+  addBookToLibrary(book);
+});
+
+addBook.addEventListener("click", () => openForm());
+
+popup.addEventListener("click", (e) => {
+  if (
+    e.target.className === "popup-container" ||
+    e.target.classList.contains("close-popup")
+  ) {
+    popup.style.display = "none";
+  }
+});
+
+document.onkeydown = (e) => {
+  if (e.key === "Escape") {
+    popup.style.display = "none";
+  }
+};
+
+// Submit form
 newBookForm.addEventListener("submit", (e) => {
   e.preventDefault();
   getData(e.target);
   popup.style.display = "none";
+  e.target.reset();
+});
+
+// Remove book
+document.addEventListener("click", (e) => {
+  const target = e.target.closest(".remove-book"); // Or any other selector.
+  if (target) {
+    const index = Array.prototype.indexOf.call(
+      target.parentElement.parentElement.children,
+      target.parentElement
+    );
+    myLibrary.splice(index, 1);
+    target.parentElement.remove();
+  }
+});
+
+// Toggle is-read
+document.addEventListener("click", (e) => {
+  const target = e.target.closest(".is-read"); // Or any other selector.
+  if (target) {
+    const index = Array.prototype.indexOf.call(
+      target.parentElement.parentElement.children,
+      target.parentElement
+    );
+    if (myLibrary[index].isRead === "Not Read") {
+      myLibrary[index].isRead = "Read";
+      target.textContent = "Read";
+    } else {
+      myLibrary[index].isRead = "Not Read";
+      target.textContent = "Not Read";
+    }
+  }
 });
