c17623bc12e2c629e3fe41b05d9ab4030efc4672
bret-henderson <98489557+bret-henderson@users.noreply.github.com>
2023-02-13 12:05:08 -0500

    Change to single quotes

--- javascript.js
+++ javascript.js
@@ -1,54 +1,54 @@
 const myLibrary = [];
-const cardContainer = document.querySelector(".card-container");
+const cardContainer = document.querySelector('.card-container');
 
-const addBook = document.querySelector(".add-book");
-const popup = document.querySelector(".popup-container");
+const addBook = document.querySelector('.add-book');
+const popup = document.querySelector('.popup-container');
 
-const newBookForm = document.querySelector(".new-book-form");
+const newBookForm = document.querySelector('.new-book-form');
 
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  let readStr = "";
-  if (read === true) readStr = "Read";
-  else readStr = "Not Read";
+  let readStr = '';
+  if (read === true) readStr = 'Read';
+  else readStr = 'Not Read';
   this.isRead = readStr;
 }
 
 function addBookToLibrary(newBook) {
-  const card = document.createElement("div");
-  card.classList.add("card");
+  const card = document.createElement('div');
+  card.classList.add('card');
   cardContainer.appendChild(card);
 
-  const title = document.createElement("h1");
-  title.classList.add("title");
+  const title = document.createElement('h1');
+  title.classList.add('title');
   title.textContent = newBook.title;
   card.appendChild(title);
 
-  const author = document.createElement("h2");
-  author.classList.add("author");
+  const author = document.createElement('h2');
+  author.classList.add('author');
   author.textContent = newBook.author;
   card.appendChild(author);
 
-  const pages = document.createElement("div");
-  pages.classList.add("pages");
+  const pages = document.createElement('div');
+  pages.classList.add('pages');
   pages.textContent = `${newBook.pages} pages`;
   card.appendChild(pages);
 
-  const isRead = document.createElement("button");
-  isRead.classList.add("is-read");
+  const isRead = document.createElement('button');
+  isRead.classList.add('is-read');
   isRead.textContent = newBook.isRead;
   card.appendChild(isRead);
 
-  const removeBookBtn = document.createElement("button");
-  removeBookBtn.classList.add("remove-book");
-  removeBookBtn.textContent = "Remove Book";
+  const removeBookBtn = document.createElement('button');
+  removeBookBtn.classList.add('remove-book');
+  removeBookBtn.textContent = 'Remove Book';
   card.appendChild(removeBookBtn);
 }
 
 function openForm() {
-  popup.style.display = "flex";
+  popup.style.display = 'flex';
 }
 
 function submitForm(formData) {
@@ -57,7 +57,7 @@ function submitForm(formData) {
     formDataObj.title,
     formDataObj.author,
     formDataObj.pages,
-    formDataObj["read-checkbox"] === "on"
+    formDataObj['read-checkbox'] === 'on'
   );
   myLibrary.push(newBook);
   addBookToLibrary(newBook);
@@ -68,8 +68,8 @@ function getData(form) {
   submitForm(formData);
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", true);
-const dune = new Book("Dune", "Frank Herbert", "412", false);
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', true);
+const dune = new Book('Dune', 'Frank Herbert', '412', false);
 
 myLibrary.push(theHobbit);
 myLibrary.push(dune);
@@ -78,34 +78,34 @@ myLibrary.forEach((book) => {
   addBookToLibrary(book);
 });
 
-addBook.addEventListener("click", () => openForm());
+addBook.addEventListener('click', () => openForm());
 
-popup.addEventListener("click", (e) => {
+popup.addEventListener('click', (e) => {
   if (
-    e.target.className === "popup-container" ||
-    e.target.classList.contains("close-popup")
+    e.target.className === 'popup-container' ||
+    e.target.classList.contains('close-popup')
   ) {
-    popup.style.display = "none";
+    popup.style.display = 'none';
   }
 });
 
 document.onkeydown = (e) => {
-  if (e.key === "Escape") {
-    popup.style.display = "none";
+  if (e.key === 'Escape') {
+    popup.style.display = 'none';
   }
 };
 
 // Submit form
-newBookForm.addEventListener("submit", (e) => {
+newBookForm.addEventListener('submit', (e) => {
   e.preventDefault();
   getData(e.target);
-  popup.style.display = "none";
+  popup.style.display = 'none';
   e.target.reset();
 });
 
 // Remove book
-document.addEventListener("click", (e) => {
-  const target = e.target.closest(".remove-book"); // Or any other selector.
+document.addEventListener('click', (e) => {
+  const target = e.target.closest('.remove-book'); // Or any other selector.
   if (target) {
     const index = Array.prototype.indexOf.call(
       target.parentElement.parentElement.children,
@@ -117,19 +117,19 @@ document.addEventListener("click", (e) => {
 });
 
 // Toggle is-read
-document.addEventListener("click", (e) => {
-  const target = e.target.closest(".is-read"); // Or any other selector.
+document.addEventListener('click', (e) => {
+  const target = e.target.closest('.is-read'); // Or any other selector.
   if (target) {
     const index = Array.prototype.indexOf.call(
       target.parentElement.parentElement.children,
       target.parentElement
     );
-    if (myLibrary[index].isRead === "Not Read") {
-      myLibrary[index].isRead = "Read";
-      target.textContent = "Read";
+    if (myLibrary[index].isRead === 'Not Read') {
+      myLibrary[index].isRead = 'Read';
+      target.textContent = 'Read';
     } else {
-      myLibrary[index].isRead = "Not Read";
-      target.textContent = "Not Read";
+      myLibrary[index].isRead = 'Not Read';
+      target.textContent = 'Not Read';
     }
   }
 });
